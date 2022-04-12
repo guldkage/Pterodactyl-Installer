fa56fa1e7f2a26ddc3df88847c768ebc08b83b63
guldkage <60321323+guldkage@users.noreply.github.com>
2022-04-12 12:43:22 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -97,7 +97,7 @@ extra() {
         sed -i -e "s@<user>@www-data@g" /etc/systemd/system/pteroq.service
         sudo systemctl enable --now redis-server
         sudo systemctl enable --now pteroq.service
-    if  [ "$lsb_dist" =  "centos" ]; then
+    else if  [ "$lsb_dist" =  "centos" ]; then
         chown -R nginx:nginx /var/www/pterodactyl/*
         curl -o /etc/systemd/system/pteroq.service https://raw.githubusercontent.com/guldkage/Pterodactyl-Installer/main/config/pteroq.service
         * * * * * php /var/www/pterodactyl/artisan schedule:run >> /dev/null 2>&1
