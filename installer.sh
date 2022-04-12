57fa044c498346b015d1d257fefd0525d94418b3
guldkage <60321323+guldkage@users.noreply.github.com>
2022-04-12 14:06:06 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -305,5 +305,4 @@ output ""
 output "This script is not resposible for any damages. The script has been tested several times without issues."
 output "Support is not given."
 output "This script will only work on a fresh installation. Proceed with caution if not having a fresh installation"
-sleep 3s
 start
