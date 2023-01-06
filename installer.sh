72013fd65d7e9235ed66e44eba5c9775fe749bbf
Malthe <60321323+guldkage@users.noreply.github.com>
2023-01-06 12:45:21 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -512,7 +512,7 @@ required(){
     output "Installing packages..."
     output "This may take a while."
     output ""
-    if  [ "$dist" =  "ubuntu" ] || [ "$dist" =  "debian" ]; then
+    if  [ "$dist" =  "ubuntu" ]; then
         apt-get update
         apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
         output "Installing dependencies"
@@ -528,6 +528,24 @@ required(){
         output "Installing PHP, MariaDB and NGINX"
         sleep 1s
         apt -y install php8.1 php8.1-{cli,gd,mysql,pdo,mbstring,tokenizer,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+        database
+    elif  [ "$dist" =  "debian" ]; then
+        apt-get update
+        apt -y install software-properties-common curl ca-certificates gnupg2 sudo lsb-release
+        output "Installing dependencies"
+        sleep 1s
+        echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+        curl -fsSL  https://packages.sury.org/php/apt.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/sury-keyring.gpg
+        curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+        echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+        apt update -y
+        
+        apt-add-repository universe
+        apt install certbot python3-certbot-nginx -y
+        output "Installing PHP, MariaDB and NGINX"
+        sleep 1s
+        curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bas
+        apt install -y php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
         database
     fi
 }
