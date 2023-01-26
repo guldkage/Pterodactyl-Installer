ea45fe23077a9156cf50e32863e66896e4d18249
Malthe <60321323+guldkage@users.noreply.github.com>
2023-01-26 16:36:44 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -155,6 +155,7 @@ phpmyadmininstall(){
         chmod o+w config/config.inc.php
         rm -rf /var/www/phpmyadmin/config
         phpmyadminweb
+        fi
     if  [ "$dist" =  "debian" ]; then
         apt install nginx certbot -y
         mkdir /var/www/phpmyadmin && cd /var/www/phpmyadmin || exit || output "An error occurred. Could not create directory." || exit
