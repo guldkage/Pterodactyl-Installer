711ea55325fa262307f7b3261e331a323cbd2e93
Malthe <60321323+guldkage@users.noreply.github.com>
2023-03-30 15:33:54 +0200

    Create interactive_install.sh

---
+++ interactive_install.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+#!/usr/bin/env bash
+
+########################################################################
+#                                                                      #
+#            Pterodactyl Installer, Updater, Remover and More          #
+#            Copyright 2022, Malthe K, <me@malthe.cc>                  # 
+# https://github.com/guldkage/Pterodactyl-Installer/blob/main/LICENSE  #
+#                                                                      #
+#  This script is not associated with the official Pterodactyl Panel.  #
+#  You may not remove this line                                        #
+#                                                                      #
+########################################################################
+
+dist="$(. /etc/os-release && echo "$ID")"
+
+
+### This script is meant to be used: ###
+### ./install.sh <FQDN/URL to panel> <SSL true or false> <email> <username> <firstname> <lastname> <password> <wings true or false> ###
+
+finish(){
+    clear
+    echo ""
+    echo "[!] Panel installed."
+    echo ""
+}
+
+panel_conf(){
+    [ "$SSL" == true ] && appurl="https://$FQDN"
+    [ "$SSL" == false ] && appurl="http://$FQDN"
+    DBPASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1`
+    mysql -u root -e "CREATE USER 'pterodactyl'@'127.0.0.1' IDENTIFIED BY '$DBPASSWORD';" && mysql -u root -e "CREATE DATABASE panel;" && mysql -u root -e "GRANT ALL PRIVILEGES ON panel.* TO 'pterodactyl'@'127.0.0.1' WITH GRANT OPTION;" && mysql -u root -e "FLUSH PRIVILEGES;"
+    php artisan p:environment:setup --author="$EMAIL" --url="$appurl" --timezone="CET" --telemetry=false --cache="redis" --session="redis" --queue="redis" --redis-host="localhost" --redis-pass="null" --redis-port="6379" --settings-ui=true
+    php artisan p:environment:database --host="127.0.0.1" --port="3306" --database="panel" --username="pterodactyl" --password="$DBPASSWORD"
+    php artisan migrate --seed --force
+    php artisan p:user:make --email="$EMAIL" --username="$USERNAME" --name-first="$FIRSTNAME" --name-last="$LASTNAME" --password="$PASSWORD" --admin=1
+    chown -R www-data:www-data /var/www/pterodactyl/*
+    curl -o /etc/systemd/system/pteroq.service https://raw.githubusercontent.com/guldkage/Pterodactyl-Installer/main/configs/pteroq.service
+    (crontab -l ; echo "* * * * * php /var/www/pterodactyl/artisan schedule:run >> /dev/null 2>&1")| crontab -
+    sudo systemctl enable --now redis-server
+    sudo systemctl enable --now pteroq.service
+    [ "$WINGS" == true ] && curl -sSL https://get.docker.com/ | CHANNEL=stable bash && systemctl enable --now docker && mkdir -p /etc/pterodactyl && apt-get -y install curl tar unzip && curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")" && curl -o /etc/systemd/system/wings.service https://raw.githubusercontent.com/guldkage/Pterodactyl-Installer/main/configs/wings.service && chmod u+x /usr/local/bin/wings
+    if  [ "$SSL" =  "true" ]; then
+        rm -rf /etc/nginx/sites-enabled/default
+        curl -o /etc/nginx/sites-enabled/pterodactyl.conf https://raw.githubusercontent.com/guldkage/Pterodactyl-Installer/main/configs/pterodactyl-nginx-ssl.conf
+        sed -i -e "s@<domain>@${FQDN}@g" /etc/nginx/sites-enabled/pterodactyl.conf
+        systemctl stop nginx
+        certbot certonly --standalone -d $FQDN --staple-ocsp --no-eff-email -m $EMAIL --agree-tos
+        systemctl start nginx
+        finish
+        fi
+    if  [ "$SSL" =  "false" ]; then
+        rm -rf /etc/nginx/sites-enabled/default
+        curl -o /etc/nginx/sites-enabled/pterodactyl.conf https://raw.githubusercontent.com/guldkage/Pterodactyl-Installer/main/configs/pterodactyl-nginx.conf
+        sed -i -e "s@<domain>@${FQDN}@g" /etc/nginx/sites-enabled/pterodactyl.conf
+        systemctl restart nginx
+        finish
+        fi
+}
+
+panel_install(){
+    echo "" 
+    if  [ "$dist" =  "ubuntu" ]; then
+        apt-get update
+        apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+        LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+        curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor --batch --yes -o /usr/share/keyrings/redis-archive-keyring.gpg
+        echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+        curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+        apt update
+        apt-add-repository universe
+        apt install certbot python3-certbot-nginx -y
+        apt -y install php8.1 php8.1-{cli,gd,mysql,pdo,mbstring,tokenizer,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+        curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+        mkdir /var/www/pterodactyl
+        cd /var/www/pterodactyl
+        curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
+        tar -xzvf panel.tar.gz
+        chmod -R 755 storage/* bootstrap/cache/
+        cp .env.example .env
+        command composer install --no-dev --optimize-autoloader --no-interaction
+        php artisan key:generate --force
+        panel_conf
+    elif  [ "$dist" =  "debian" ]; then
+        apt-get update
+        apt -y install software-properties-common curl ca-certificates gnupg2 sudo lsb-release
+        echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+        curl -fsSL  https://packages.sury.org/php/apt.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/sury-keyring.gpg
+        curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+        echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+        apt update -y
+        apt-add-repository universe
+        apt install certbot python3-certbot-nginx -y
+        curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bas
+        apt install -y php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+        curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+        mkdir /var/www/pterodactyl
+        cd /var/www/pterodactyl
+        curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
+        tar -xzvf panel.tar.gz
+        chmod -R 755 storage/* bootstrap/cache/
+        cp .env.example .env
+        command composer install --no-dev --optimize-autoloader --no-interaction
+        php artisan key:generate --force
+        panel_conf
+    fi
+}
+
+#! /bin/sh -
+
+FQDN=`echo $1`
+SSL=`echo $2`
+EMAIL=`echo $3`
+USERNAME=`echo $4`
+FIRSTNAME=`echo $5`
+LASTNAME=`echo $6`
+PASSWORD=`echo $7`
+WINGS=`echo $8`
+
+echo "Welcome to Interactive Install of Pterodactyl Panel"
+echo "Quick summary before the install begins:"
+echo ""
+echo "FQDN (URL): $FQDN"
+echo "SSL: $SSL"
+echo "Preselected webserver: NGINX"
+echo "Email $EMAIL"
+echo "Username $USERNAME"
+echo "First name $FIRSTNAME"
+echo "Last name $LASTNAME"
+echo "Password: $PASSWORD"
+echo "Wings install: $WINGS"
+echo ""
+echo "Starting automatic installation in 5 seconds"
+sleep 5s
+panel_install
